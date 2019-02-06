7fade5e61f524e939114b754e1f7e48a71022931
Neelanjana Mukherjee <neelanjanamukherjee@zipcoders-MacBook-Pro.local>
2019-02-06 08:20:01 -0500

    added the method to print sum of inputs

--- Main.java
+++ Main.java
@@ -1,9 +1,32 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(){
+        
+       getSum(); 
 
+    }
+    
+    public static void getSum (){
+    int i;
+    int Sum =0;
+    Scanner num = new Scanner(System.in);
+    System.out.println("Enter a number to get the sum of the numbers from 1 to your number");
+    int guess = num.nextInt(); 
+    
+    for(i=1;i<=guess;i++){
+        
+        
+        Sum =Sum+i;
+      
+        
+        
+    }
+      System.out.println("The sum of the numbers from 1 to "+guess+" is = "+Sum);  
     }
 }
